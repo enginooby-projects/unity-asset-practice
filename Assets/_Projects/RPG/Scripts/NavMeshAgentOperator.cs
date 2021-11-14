bdda05722121a747e5ea09fa498f84473040983a
enginoobz <enginoobz@gmail.com>
2021-11-14 14:29:44 +0100

    [PROJECT - RPG] Set Cinemachine Virtual Camera

--- Assets/_Projects/RPG/Scripts/NavMeshAgentOperator.cs
+++ Assets/_Projects/RPG/Scripts/NavMeshAgentOperator.cs
@@ -9,19 +9,22 @@ public class NavMeshAgentOperator : MonoBehaviour {
   [SerializeField] private Transform _target;
   [AutoRef, SerializeField, HideInInspector] private NavMeshAgent _agent;
 
-  private Ray _lastRay; // ? Separate to player
-
   void Update() {
     ProcessMoveToCursor();
   }
+
+  // ? Separate to player
+  #region CONTROL 
+  private Ray _lastRay;
 
   private void ProcessMoveToCursor() {
     Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100, Color.red);
 
     if (!MouseButton.Left.IsDown()) return;
-    _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition); // UTIL
+    _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition); //
     if (Physics.Raycast(_lastRay, out RaycastHit hit)) {
       _agent.destination = hit.point;
     }
   }
+  #endregion
 }
