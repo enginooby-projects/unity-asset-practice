d8ea53f4888522916bbba356373614d753617152
enginoobz <enginoobz@gmail.com>
2021-11-15 12:14:48 +0100

    [PROJECT - RPG] Implement attack rate

--- Assets/_Projects/RPG/Scripts/Combat/Fighter.cs
+++ Assets/_Projects/RPG/Scripts/Combat/Fighter.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using DG.Tweening;
 using Enginoobz.Operator;
 using Enginoobz.Core;
+using Sirenix.OdinInspector;
 
 namespace Project.RPG.Combat {
   public class Fighter : MonoBehaviour, IAction {
     [Tooltip("Move to target and stop at this distance to attack.")]
     [SerializeField] private float _attackRange = 2;
+
+    [SerializeField, Min(0.5f)] private float _attackRate = 1f;
 
     [AutoRef, SerializeField, HideInInspector]
     private NavMeshAgentOperator _agentOpr;
@@ -19,9 +22,10 @@ namespace Project.RPG.Combat {
 
     private CombatTarget _currentTarget;
     private bool _isAttacking;
+    private float timeSinceLastAttack;
 
     private void Update() {
-      if (_isAttacking) AttackCurrentTarget();
+      if (_isAttacking) ApproachAndAttackCurrentTarget();
     }
 
     public void Attack(CombatTarget target) {
@@ -29,17 +33,24 @@ namespace Project.RPG.Combat {
       _currentTarget = target;
     }
 
-    public void AttackCurrentTarget() {
+    public void ApproachAndAttackCurrentTarget() {
       if (transform.DistanceFrom(_currentTarget.transform) > _attackRange) {
         // FIX: agent does not guarantee to arrive at the range (e.g target is on the air)
         _agentOpr.MoveTo(_currentTarget.transform.position, _attackRange);
       } else {
-        _animator.SetTrigger(attackHash);
-        // transform.DOLookAt(_currentTarget.transform.position, 1).OnComplete(() => {
-        //   // print("Attacking " + _currentTarget.name);
-        //   // _animator.SetBool(_isAttackingHash, true);
-        // });
+        if (Time.time - timeSinceLastAttack > _attackRate) {
+          AttackCurrentTarget();
+          timeSinceLastAttack = Time.time;
+        }
       }
+    }
+
+    private void AttackCurrentTarget() {
+      _animator.SetTrigger(attackHash);
+      // transform.DOLookAt(_currentTarget.transform.position, 1).OnComplete(() => {
+      //   // print("Attacking " + _currentTarget.name);
+      //   // _animator.SetBool(_isAttackingHash, true);
+      // });
     }
 
     // animation events
