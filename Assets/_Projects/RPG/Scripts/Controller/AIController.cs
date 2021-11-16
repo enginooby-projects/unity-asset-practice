0a91276432644352f70f0e6bb7e87317a12f3251
enginoobz <enginoobz@gmail.com>
2021-11-16 11:50:49 +0100

    [PROJECT - RPG] Implement guarding behavior for enemy

--- Assets/_Projects/RPG/Scripts/Controller/AIController.cs
+++ Assets/_Projects/RPG/Scripts/Controller/AIController.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using Project.RPG.Combat;
+using Enginoobz.Operator;
 
 namespace Project.RPG.Controller {
   public class AIController : MonoBehaviour {
-    [AutoRef, SerializeField, HideInInspector]
-    private Fighter _fighter;
-
+    [AutoRef, SerializeField, HideInInspector] private Fighter _fighter;
+    [AutoRef, SerializeField, HideInInspector] private NavMeshAgentOperator _agentOpr;
     [SerializeField] private Reference _playerRef;
+    [SerializeField, HideLabel] private AreaCircular _vision = new AreaCircular(label: "Vision", radius: 5, angle: 90);
 
-    [SerializeField, HideLabel]
-    private AreaCircular _vision = new AreaCircular(label: "Vision", radius: 5, angle: 90);
+    private Vector3 guardPos;
+    private bool isReturningToGuardPos;
 
+    private void Start() {
+      guardPos = transform.position;
+    }
 
     void Update() {
       if (_vision.Contains(_playerRef)) {
         _fighter.Attack(_playerRef);
+        isReturningToGuardPos = false;
       } else {
-        _fighter.Cancel();
+        HandleGuarding();
       }
     }
 
+    private void HandleGuarding() {
+      _fighter.Cancel();
+      if (!isReturningToGuardPos) {
+        _agentOpr.MoveTo(guardPos);
+        isReturningToGuardPos = true;
+      }
+    }
+
+#if UNITY_EDITOR
     private void OnDrawGizmos() {
       _vision.DrawGizmos();
     }
   }
+#endif
 }
