90561618302cbfb6da66af8c2fcc03c04c364d7d
enginoobz <enginoobz@gmail.com>
2021-11-16 16:28:37 +0100

    [PROJECT - RPG] Implement patrolling behavior for enemy using SWS

--- Assets/_Projects/RPG/Scripts/Controller/AIController.cs
+++ Assets/_Projects/RPG/Scripts/Controller/AIController.cs
@@ -4,40 +4,44 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 using Project.RPG.Combat;
 using Enginoobz.Operator;
+using SWS;
 
 namespace Project.RPG.Controller {
   public class AIController : MonoBehaviour {
     [SerializeField] private Reference _playerRef;
     [SerializeField, HideLabel] private AreaCircular _vision = new AreaCircular(label: "Vision", radius: 5, angle: 90);
 
-    [Tooltip("Duration enemy stay in place before returning to guarding point when lose tracking of player.")]
+    [Tooltip("Duration enemy stay in place before return patrolling when lose track of player.")]
     [SerializeField] private Vector2Wrapper _suspiciousTime = new Vector2Wrapper(new Vector2(2, 5), 0, 10);
 
     [AutoRef, SerializeField, HideInInspector] private Fighter _fighter;
+    [AutoRef, SerializeField, HideInInspector] private navMove _navMover; // ? Create a wrapper for SWS.navMove
     [AutoRef, SerializeField, HideInInspector] private NavMeshAgentOperator _agentOpr;
 
-    private Vector3 guardPos;
-    private bool isReturningToGuardPos;
-
-    private void Start() {
-      guardPos = transform.position;
-    }
+    private bool isPatrolling;
 
     void Update() {
       if (_vision.Contains(_playerRef)) {
         _fighter.Attack(_playerRef);
-        isReturningToGuardPos = false;
+        isPatrolling = false;
+        _navMover.Pause();
       } else {
-        HandleGuarding();
+        HandlePatrolling();
       }
     }
 
-    private void HandleGuarding() {
+    private void HandlePatrolling() {
       _fighter.Cancel();
-      if (!isReturningToGuardPos) {
-        _agentOpr.MoveTo(guardPos, delay: _suspiciousTime.Random);
-        isReturningToGuardPos = true;
+      if (!isPatrolling) {
+        _navMover.Resume();
+        isPatrolling = true;
       }
+    }
+
+    public void OnDead() {
+      _navMover.Stop();
+      _fighter.Cancel();
+      Destroy(this);
     }
 
 #if UNITY_EDITOR
