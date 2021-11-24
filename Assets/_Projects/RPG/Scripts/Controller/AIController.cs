1f2a45baaca466d0707f90157bc042d622cd09b6
enginoobz <enginoobz@gmail.com>
2021-11-24 13:51:06 +0100

    [PROJECT - RPG] Implement aggravation when get attacked for enemy

--- Assets/_Projects/RPG/Scripts/Controller/AIController.cs
+++ Assets/_Projects/RPG/Scripts/Controller/AIController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using Project.RPG.Combat;
@@ -8,25 +7,51 @@ using SWS;
 
 namespace Project.RPG.Controller {
   public class AIController : MonoBehaviour {
-    [SerializeField] private Reference _playerRef;
-    [SerializeField, HideLabel] private AreaCircular _vision = new AreaCircular(label: "Vision", radius: 5, angle: 90);
+    [SerializeField]
+    private Reference _playerRef;
+
+    [SerializeField, HideLabel]
+    private AreaCircular _vision = new AreaCircular(label: "Vision", radius: 5, angle: 90);
 
     [Tooltip("Duration enemy stay in place before return patrolling when lose track of player.")]
-    [SerializeField] private Vector2Wrapper _suspiciousTime = new Vector2Wrapper(new Vector2(2, 5), 0, 10);
-    [SerializeField] private float _patrolSpeed = 2f;
+    [SerializeField]
+    private Vector2Wrapper _suspiciousTime = new Vector2Wrapper(new Vector2(2, 5), 0, 10);
 
-    [AutoRef, SerializeField, HideInInspector] private Fighter _fighter;
-    [AutoRef, SerializeField, HideInInspector] private navMove _navMover; // ? Create a wrapper for SWS.navMove
-    [AutoRef, SerializeField, HideInInspector] private NavMeshAgentOperator _agentOpr;
+    [SerializeField]
+    private Vector2 _aggravationTime = new Vector2(3f, 5f);
+
+    [SerializeField]
+    private float _patrolSpeed = 2f;
+
+    [AutoRef, SerializeField, HideInInspector]
+    private Fighter _fighter;
+
+    [AutoRef, SerializeField, HideInInspector]
+    private navMove _navMover; // ? Create a wrapper for SWS.navMove
+
+    [AutoRef, SerializeField, HideInInspector]
+    private NavMeshAgentOperator _agentOpr;
 
     private bool isPatrolling;
+    private bool _isAggravated;
+
 
     void Update() {
-      if (_vision.Contains(_playerRef)) {
+      if (_vision.Contains(_playerRef) || _isAggravated) {
         HandleAttacking();
       } else {
         HandlePatrolling();
       }
+    }
+
+    public void Aggravate() {
+      _isAggravated = true;
+      StartCoroutine(StopAggravationCoroutine(_aggravationTime.Random()));
+    }
+
+    private IEnumerator StopAggravationCoroutine(float delay) {
+      yield return new WaitForSeconds(delay);
+      _isAggravated = false;
     }
 
     private void HandleAttacking() {
